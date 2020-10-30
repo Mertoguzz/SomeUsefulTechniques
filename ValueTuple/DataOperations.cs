7ea7ea58dc27ec9ba08a267513b5ec7395d95d1c
MUSTAFA MERT OGUZ <EXT02D19216@turkcell.entp.tgc>
2020-10-30 18:22:34 +0300

    Add project files.

---
+++ ValueTuple/DataOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueTuple
+{
+    public class DataOperations
+    {
+      
+        public (string firstname, string lastname) GetFullName()
+        {
+            return ("Mert", "OGUZ");
+        }
+    }
+}
---
+++ ValueTuple/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueTuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This feature was added in .Net Framework 4.7.
+            // it is the simplest form of making Tuple
+            //ValueTuple
+            DataOperations dataOperations = new DataOperations();
+            var fullname=dataOperations.GetFullName();
+            Console.WriteLine(string.Format("{0} {1}", fullname.firstname, fullname.lastname));
+            Console.ReadKey();
+        }
+    }
+}
